a8dfe1b28140e6ca45744ae30875966d5b48e1bf
Nikhil Babu <nikhilbabu80942@gmail.com>
2024-06-24 14:38:36 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all available IPv4 addresses
+    port: 3000, // Specify the port you want to use
+  },
+});
